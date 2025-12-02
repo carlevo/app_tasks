b029b29599f34ea0149108a7ad4f8609090c5964
Carlos <carloscreus@ceroca.cat>
2025-12-02 16:08:28 +0100

    Estil dialog acabat

--- lib/components/dialog_nova_tasca.dart
+++ lib/components/dialog_nova_tasca.dart
@@ -1,5 +1,6 @@
 import 'package:app_tasks/colors_app.dart';
 import 'package:app_tasks/components/boto_dialog.dart';
+import 'package:app_tasks/components/textfieldpersonalitzat.dart';
 import 'package:flutter/material.dart';
 
 class DialogNovaTasca extends StatelessWidget {
@@ -8,18 +9,28 @@ class DialogNovaTasca extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      backgroundColor: ColorsApp.secondaryColor,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      //Fondo del dialogo
+      backgroundColor: ColorsApp.accentColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+        //Añadimos el side
+        side: BorderSide(color: ColorsApp.secondaryColor, width: 2),
+      ),
 
       title: Text(
         "Quina nova tasca vols afegir?",
         style: TextStyle(color: ColorsApp.primaryColor),
       ),
       content: Container(
+        //Añadimos el height y width para limitar el contenido
+        height: 150,
+        width: MediaQuery.of(context).size.width * 0.8,
         child: Column(
+          //Añadimos un evenly para que quede uniforme
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           mainAxisSize: MainAxisSize.min,
           children: [
-            TextField(),
+            Textfieldpersonalitzat(),
             SizedBox(height: 20),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
